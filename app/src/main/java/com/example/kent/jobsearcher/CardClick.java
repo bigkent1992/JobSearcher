8039c07339e7242929c9c8b1ba61f00fc85645e5
Alexander Struchinsky <bigkent1992@gmail.com>
2017-08-15 14:06:38 +0300

    New update

---
+++ app/src/main/java/com/example/kent/jobsearcher/CardClick.java
@@ -0,0 +1,11 @@
+package com.example.kent.jobsearcher;
+
+import com.example.kent.jobsearcher.Model.Vacancy;
+
+/**
+ * Created by User on 14.08.2017.
+ */
+
+public interface CardClick {
+    void onCardClick(Vacancy vacancy);
+}
